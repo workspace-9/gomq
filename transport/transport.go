436f352d27e98549fa590f113554909eda1bdfbe
Jordan Bonecutter <i@jordanbonecutter.com>
2023-10-21 16:04:56 -0700

    Change wording

--- transport/transport.go
+++ transport/transport.go
@@ -7,7 +7,7 @@ import (
 
 // Transport represents a method of generating sockets.
 type Transport interface {
-	// Bind creates an Acceptor if successful.
+	// Bind creates an Listener if successful.
 	Bind(addr string) (net.Listener, error)
 
 	// Connect to a remote address.
