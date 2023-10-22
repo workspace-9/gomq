36f2a95021147f9c215b3628945ba3eac3326453
Jordan Bonecutter <i@jordanbonecutter.com>
2023-10-22 14:53:47 -0700

    remove unused file

--- driver.go
+++
@@ -1,8 +0,0 @@
-package gomq
-
-// driver is responsible for implementing socket operations.
-type driver interface {
-  sendMessage([][]byte) error
-  recvMessage() ([][]byte, error)
-  typ() SocketType
-}
