4baa089e45c076ff8a078007c0ad970b5dafd45c
Jordan Bonecutter <i@jordanbonecutter.com>
2023-10-25 22:59:52 -0700

    trie, test

--- types/pub/trie/trie.go
+++ types/pub/trie/trie.go
@@ -8,13 +8,17 @@ import (
 type Trie[T comparable] struct {
 	Key      string
 	Children map[string]*Trie[T]
-	Values   []T
+	Values   map[T]struct{}
 }
 
 // Put the value in the trie at the given key.
 func (t *Trie[T]) Put(key string, value T) {
 	if key == t.Key {
-		t.Values = append(t.Values, value)
+		if t.Values == nil {
+			t.Values = make(map[T]struct{})
+		}
+		t.Values[value] = struct{}{}
+		return
 	}
 
 	var newChildren map[string]*Trie[T]
@@ -27,7 +31,7 @@ func (t *Trie[T]) Put(key string, value T) {
 				newChildren[childKey] = child
 				delete(t.Children, childKey)
 			}
-		} else if len(key) <= len(childKey) {
+		} else if len(key) >= len(childKey) {
 			if strings.HasPrefix(key, childKey) {
 				child.Put(key, value)
 				return
@@ -35,25 +39,61 @@ func (t *Trie[T]) Put(key string, value T) {
 		}
 	}
 
+	if t.Children == nil {
+		t.Children = make(map[string]*Trie[T])
+	}
+
 	if newChildren != nil {
 		t.Children[key] = &Trie[T]{
-			Key: key, Children: newChildren, Values: []T{value},
+			Key: key, Children: newChildren, Values: map[T]struct{}{value: {}},
 		}
 	} else {
 		t.Children[key] = &Trie[T]{
-			Key: key, Children: make(map[string]*Trie[T]), Values: []T{value},
+			Key: key, Values: map[T]struct{}{value: {}},
 		}
 	}
 }
 
-func (t *Trie[T]) Search(prefix string, visitFunc func(key string, value T)) {
-	for _, val := range t.Values {
-		visitFunc(t.Key, val)
+func (t *Trie[T]) Remove(key string, value T) bool {
+	return t.removeParent(key, value, nil)
+}
+
+func (t *Trie[T]) removeParent(key string, value T, parent *Trie[T]) bool {
+	if key == t.Key {
+		_, ok := t.Values[value]
+		if ok {
+			delete(t.Values, value)
+		}
+
+		if len(t.Values) == 0 {
+			delete(parent.Children, t.Key)
+			for _, child := range t.Children {
+				parent.Children[child.Key] = child
+			}
+		}
+
+		return ok
 	}
 
 	for childKey, child := range t.Children {
-		if strings.HasPrefix(childKey, prefix) {
-			child.Search(prefix, visitFunc)
+		if strings.HasPrefix(key, childKey) {
+			return child.removeParent(key, value, t)
 		}
+	}
+
+	return false
+}
+
+func (t *Trie[T]) Query(query string, visitFunc func(key string, value T)) {
+	if !strings.HasPrefix(query, t.Key) {
+		return
+	}
+
+	for value := range t.Values {
+		visitFunc(t.Key, value)
+	}
+
+	for _, child := range t.Children {
+		child.Query(query, visitFunc)
 	}
 }
---
+++ types/pub/trie/trie_test.go
@@ -0,0 +1,150 @@
+package trie_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/exe-or-death/gomq/types/pub/trie"
+	"testing"
+)
+
+func TestTrieBasic(t *testing.T) {
+	trie := &trie.Trie[string]{}
+	trie.Put("", "a")
+	if err := ExpectStructure(`""`, trie); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	trie.Put("foo", "b")
+	if err := ExpectStructure(`{"": "foo"}`, trie); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	trie.Put("foobar", "c")
+	if err := ExpectStructure(`{"": {"foo": "foobar"}}`, trie); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	trie.Put("bazfoo", "d")
+	if err := ExpectStructure(`{"": {"foo": "foobar", "bazfoo": 0}}`, trie); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	trie.Put("baz", "e")
+	if err := ExpectStructure(`{"": {"foo": "foobar", "baz": "bazfoo"}}`, trie); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expect := ExpectToVisit("a")
+	trie.Query("", expect.Visit)
+	if err := expect.Error(); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expect = ExpectToVisit("a", "b")
+	trie.Query("foo", expect.Visit)
+	if err := expect.Error(); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expect = ExpectToVisit("a", "e")
+	trie.Query("baz", expect.Visit)
+	if err := expect.Error(); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	trie.Remove("foo", "b")
+	if err := ExpectStructure(`{"": {"foobar": 0, "baz": "bazfoo"}}`, trie); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expect = ExpectToVisit("a")
+	trie.Query("foo", expect.Visit)
+	if err := expect.Error(); err != nil {
+		t.Errorf(err.Error())
+	}
+}
+
+func ExpectStructure(s string, trie *trie.Trie[string]) error {
+	var data any
+	if err := json.Unmarshal([]byte(s), &data); err != nil {
+		panic(err)
+	}
+
+	return expectPath(data, "", trie)
+}
+
+func expectPath(anyData any, path string, trie *trie.Trie[string]) error {
+	switch data := anyData.(type) {
+	case float64:
+		return fmt.Errorf("Expected nothing at path %s, found %s", path, trie.Key)
+	case string:
+		if data != trie.Key {
+			return fmt.Errorf("Expected to find key %s at path %s, found %s", data, path, trie.Key)
+		}
+		return nil
+	case map[string]any:
+		if val, ok := data[trie.Key]; !ok {
+			return fmt.Errorf("Couldn't find key %s at path %s", trie.Key, path)
+		} else {
+			for _, child := range trie.Children {
+				if err := expectPath(val, path+"::"+trie.Key, child); err != nil {
+					return err
+				}
+			}
+
+			switch v := val.(type) {
+			case map[string]any:
+				if len(v) != len(trie.Children) {
+					return fmt.Errorf("Expected %d children at path %s, got %d", len(v), path, len(trie.Children))
+				}
+				return nil
+			case string:
+				if len(trie.Children) != 1 {
+					return fmt.Errorf("Expected 1 child at path %s, got %d", path, len(trie.Children))
+				}
+				return nil
+			case float64:
+				if len(trie.Children) != 0 {
+					return fmt.Errorf("Expected 0 children at path %s, got %d", path, len(trie.Children))
+				}
+				return nil
+			}
+		}
+	}
+
+	return nil
+}
+
+func ExpectToVisit(values ...string) expectToVisit {
+	m := make(map[string]int)
+	for _, v := range values {
+		m[v] = m[v] + 1
+	}
+	return expectToVisit{
+		m, make(map[string]int),
+	}
+}
+
+type expectToVisit struct {
+	expectations map[string]int
+	visited      map[string]int
+}
+
+func (e expectToVisit) Visit(_ string, value string) {
+	fmt.Println(value)
+	e.visited[value] = e.visited[value] + 1
+}
+
+func (e expectToVisit) Error() error {
+	if len(e.expectations) != len(e.visited) {
+		return fmt.Errorf("Expected to visit %d unique values, got %d", len(e.expectations), len(e.visited))
+	}
+
+	for key, visitations := range e.visited {
+		if e.expectations[key] != visitations {
+			return fmt.Errorf("Expected to visit %s %d times, got %d", key, e.expectations[key], visitations)
+		}
+	}
+
+	return nil
+}
